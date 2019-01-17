7d0855bef53aa835a7bc2e8cfd2a1a7e63d0beb9
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-01-17 22:43:04 +0100

    c#

---
+++ dotnet-netfx/csharp/dotnet-internals.cs
@@ -0,0 +1,13 @@
+# .NET Internals
+
+dotnet-internals.cs
+
+*   http://mattwarren.org/2018/01/22/Resources-for-Learning-about-.NET-Internals/
+
+*   http://www.philosophicalgeek.com/
+
+*   http://www.philosophicalgeek.com/2014/09/29/digging-into-net-object-allocation-fundamentals/
+
+*   http://www.philosophicalgeek.com/2014/11/20/digging-into-net-loop-performance-bounds-checking-iteration-and-unrolling/
+
+*   http://mattwarren.org/2018/01/22/Resources-for-Learning-about-.NET-Internals/
---
+++ dotnet-netfx/csharp/tuple-and-valuetuple.md
@@ -0,0 +1,10 @@
+# Tuple and ValueTuple
+
+tuple-and-valuetuple.md 
+
+*   https://blogs.msdn.microsoft.com/mazhou/2017/05/26/c-7-series-part-1-value-tuples/
+
+*   https://www.c-sharpcorner.com/article/working-with-value-tuple-in-c-sharp-7-0/
+
+*   https://www.grapecity.com/en/blogs/tackling-tuples-understanding-the-new-c-7-value-type
+
